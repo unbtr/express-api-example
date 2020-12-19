58a28362e77797cb863f4153a0a403439aed218b
unbtr <mail@m.tknrh.me>
2020-12-20 00:00:21 +0900

    製品APIのルーティングを追加

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import productsRoutes from './routes/produsts';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use('/products', productsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Example API listening at http://localhost:${PORT}`);
