365b3f615f7a3ec229a2ff362f3fce862ee04a49
unbtr <mail@m.tknrh.me>
2020-12-24 00:13:13 +0900

    update

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -7,3 +7,8 @@ export const index: RequestHandler = (req, res, next) => {
 export const store: RequestHandler = (req, res, next) => {
   return res.json({ value: 'create' });
 };
+
+export const update: RequestHandler<{ id: string }> = (req, res, next) => {
+  const id = req.params.id;
+  res.json({ message: '更新しました' });
+};
