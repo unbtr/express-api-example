0e84647a957d03eedcb92f59b889326a3de8ba76
unbtr <mail@m.tknrh.me>
2020-12-30 23:45:25 +0900

    ステータス追加

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -10,7 +10,7 @@ export const store: RequestHandler = (req, res, next) => {
   const name = (req.body as { name: string }).name;
   const price = (req.body as { price: number }).price;
   const product = new Product(currentID, name, price);
-  res.json({ message: 'created', product });
+  res.status(201).json({ message: 'created', product });
 };
 
 export const update: RequestHandler<{ product: string }> = (req, res, next) => {
